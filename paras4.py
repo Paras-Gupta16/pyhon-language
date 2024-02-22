ddb5ac6a27b13b408077043f0f1760df27236a8a
Paras-Gupta16 <paras94904@gmail.com>
2024-02-22 23:29:23 +0530

    Program to count the number of notes

---
+++ paras4.py
@@ -0,0 +1,17 @@
+a = int(input("enter the amount:"))
+
+c2000 = a // 2000
+a %= 2000
+
+c500 = a // 500
+a %= 500
+
+c200 = a // 200
+a %= 200
+
+c100 = a // 100
+
+print("2000 notes:", c2000)
+print("500 notes:", c500)
+print("200 notes:", c200)
+print("100 notes:", c100)
